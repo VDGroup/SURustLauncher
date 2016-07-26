d9ac7df0bc8969861e617ba7ea366932d7a9d694
vojmar <vojmar@DESKTOP-4Q08NOF>
2016-07-26 23:19:40 +0200

    Solution Created added initial projects

---
+++ RustLauncher/RustLauncher/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RustLauncher
+{
+    public class Class1
+    {
+    }
+}
